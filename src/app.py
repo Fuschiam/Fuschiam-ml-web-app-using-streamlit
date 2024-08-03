27515df0b58f80c1a80192ab06ee6be6891dd5f5
Fuschiam <fuschiamckoy@gmail.com>
2024-08-03 03:01:28 +0000

    Removed images

--- src/app.py
+++ src/app.py
@@ -98,8 +98,8 @@ if st.button("Predict"):
 # Create columns
 col1, col2 = st.columns(2)
 
-# # Display images in columns
-with col1:
-    st.image("/workspaces/Fuschiam-ml-web-app-using-streamlit/data/raw/irwan-rbDE93-0hHs-unsplash.jpg", use_column_width=True)
-with col2:
-    st.image("/workspaces/Fuschiam-ml-web-app-using-streamlit/data/raw/kenny-eliason-5ddH9Y2accI-unsplash.jpg", use_column_width=True)
+# # # Display images in columns
+# with col1:
+#     st.image("/workspaces/Fuschiam-ml-web-app-using-streamlit/data/raw/irwan-rbDE93-0hHs-unsplash.jpg", use_column_width=True)
+# with col2:
+#     st.image("/workspaces/Fuschiam-ml-web-app-using-streamlit/data/raw/kenny-eliason-5ddH9Y2accI-unsplash.jpg", use_column_width=True)
